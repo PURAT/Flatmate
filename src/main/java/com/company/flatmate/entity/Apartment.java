ee7f67b65cb29f55f1442ae3c01f0946483ad7f2
PURAT <alex-shtunder@yandex.ru>
2021-12-22 12:33:25 +0300

    Fixed port

--- src/main/java/com/company/flatmate/entity/Apartment.java
+++ src/main/java/com/company/flatmate/entity/Apartment.java
@@ -49,10 +49,6 @@ public class Apartment {
     @JoinColumn(name = "apartment_id", foreignKey = @ForeignKey(name = "apartment_id"))
     private List<Renter> renters;
 
-//    @OneToMany(targetEntity = ApartmentFeedback.class,
-//            mappedBy = "apartment", cascade = CascadeType.ALL)
-//    private List<ApartmentFeedback> feedbacks;
-
     @Column(name = "price")
     private Double price;
 
